10987cb68baa7d132b0e3f13543534f6470c8778
elkarm <75398467+elkarm@users.noreply.github.com>
2021-12-31 18:30:50 +0000

    update of build_model function
    as per comments the build_model function is updated to contain a variety of parameters that the model can be tuned on with the use of the GridSearchCV.

--- Project_2-Disaster_Response/models/train_classifier.py
+++ Project_2-Disaster_Response/models/train_classifier.py
@@ -111,7 +111,19 @@ def build_model():
    ])
     
 
-    return pipeline
+    # possible parameters for the pipine to be tuned
+    parameters = {
+        'tfidf__use_idf': (True, False),
+        'tfidf__smooth_idf': [True, False],
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'vect__max_features': (None, 5000, 10000),
+        'clf__estimator__n_estimators': [50, 100],
+        'clf__estimator__min_samples_split': [2, 4]
+    }
+    
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    
+    return cv
 
 
 def evaluate_model(model, X_test, y_test, category_names):
